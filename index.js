edcbc154af1b287770f15ba1137e7845edb3d710
Kelsey Fecho <kfecho30@gmail.com>
2018-09-16 23:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ function inOrder(currentNode){
 
 function findOrAdd(root, newNode){
   let current = root;
-  if (current.data === newNode.data) {
+  if (current.data === root.data) {
     return true
-  } else if (newNode.data < current.data) {
+  } else if (newNode.data < root.data) {
     current = current.left
+    if(current) {
+      findOrAdd(current, newNode)
+    } else {
+      root.left = newNode
+    }
   } else {
-
+    current = current.right
+    if(current) {
+      findOrAdd(current, newNode)
+    }
+    else {
+      root.right = newNode
+    }
   }
 }
 
