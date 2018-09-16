a56a89d0d17183035dac0d3e299f07ed405a9ca6
Kelsey Fecho <kfecho30@gmail.com>
2018-09-16 23:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function inOrder(currentNode){
   if(currentNode.left){
     inOrder(currentNode.left)
   }
-  console.log(currentNode.data)
+
   if(currentNode.right){
     inOrder(currentNode.right)
   }
